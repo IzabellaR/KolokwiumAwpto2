7a598cc3f105027414adb172c6c9452c5bc8f4d0
student <student@10.2.4.7>
2016-05-30 19:34:41 +0200

    coś

--- MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/MortgageCalculatorTest.java
+++ MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/MortgageCalculatorTest.java
@@ -3,9 +3,31 @@ package kolokwium.c.pl.edu.us;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
 
+@RunWith(Parameterized.class)
 public class MortgageCalculatorTest
-{
+{	
+	private int parameter;
+	 public AllTests(int testParameter) {
+	 this.parameter = testParameter;
+	 }
+	 
+	 @Parameters
+	 public static Collection<Object[]> data() {
+	 Object[][] data = new Object[][] { { 1 }, { 5 } };
+	 return Arrays.asList(data);
+	 }
+	 
+	 
+	 @Test
+	 public void test() {
+	 }
+	 
+	 
+	 
 	@Test
 	public void numberOfInstallmentsTest() {
 		fail("Not yet implemented");
